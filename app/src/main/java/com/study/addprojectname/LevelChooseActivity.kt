23c0397b974149f7e93fed4e43f39b565caddc95
Adam <niepoprawnyetycznie@gmail.com>
2021-04-21 14:22:58 +0200

    Create important activities, create check buttons in settings, add styles.xml, change app theme

---
+++ app/src/main/java/com/study/addprojectname/LevelChooseActivity.kt
@@ -0,0 +1,11 @@
+package com.study.addprojectname
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelChooseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_choose)
+    }
+}
