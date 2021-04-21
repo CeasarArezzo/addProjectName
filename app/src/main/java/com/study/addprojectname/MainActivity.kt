ef75fcc73b63a62e86ad90d7710767a2f2fee657
CeasarArezzo <ceasararezzo@gmail.com>
2021-04-22 01:18:35 +0200

    merge startActivity functions in MainActivity into one

--- app/src/main/java/com/study/addprojectname/MainActivity.kt
+++ app/src/main/java/com/study/addprojectname/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,26 +13,26 @@ class MainActivity : AppCompatActivity() {
         //second commit
     }
 
-
-    fun turnOnSettingsMenu (view: View) {
-        val intent = Intent(this, SettingsActivity::class.java)
-        startActivity(intent)
-
-    }
-
-    fun turnOnTutorial (view: View) {
-        val intent = Intent(this, TutorialActivity::class.java)
-        startActivity(intent)
-
-    }
-    fun turnOnLevelChoose (view: View) {
-        val intent = Intent(this, LevelChooseActivity::class.java)
-        startActivity(intent)
-
-    }
-    fun turnOnQuickGame (view: View) {
-        val intent = Intent(this, GameActivity::class.java)
-        startActivity(intent)
-
+    fun buttonPressed(view : View)
+    {
+        when (view.id)
+        {
+            R.id.quickGameButton ->{
+                val intent = Intent(this, GameActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.chooseLevelButton ->{
+                val intent = Intent(this, LevelChooseActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.tutorialButton ->{
+                val intent = Intent(this, TutorialActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.settingsButton ->{
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
