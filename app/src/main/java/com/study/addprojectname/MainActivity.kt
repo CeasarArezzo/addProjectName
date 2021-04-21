471bd1020d115bb34153a72c6aa610d308ccd895
Adam <niepoprawnyetycznie@gmail.com>
2021-04-21 14:33:36 +0200

    Commit everything

--- app/src/main/java/com/study/addprojectname/MainActivity.kt
+++ app/src/main/java/com/study/addprojectname/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.study.addprojectname
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //added comments
         //second commit
+    }
+
+
+    fun turnOnSettingsMenu (view: View) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivityForResult(intent, 0)
+
+    }
+
+    fun turnOnTutorial (view: View) {
+        val intent = Intent(this, TutorialActivity::class.java)
+        startActivityForResult(intent, 0)
+
+    }
+    fun turnOnLevelChoose (view: View) {
+        val intent = Intent(this, LevelChooseActivity::class.java)
+        startActivityForResult(intent, 0)
+
+    }
+    fun turnOnQuickGame (view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivityForResult(intent, 0)
+
     }
 }
